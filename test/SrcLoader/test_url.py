478ccd0dc6178e4a748f00faf5b572b2fcfba6be
thbytwo <thbytwo@live.cn>
2026-06-04 20:16:33 +0800

    demo

--- test/SrcLoader/test_url.py
+++ test/SrcLoader/test_url.py
@@ -22,19 +22,31 @@ def Test1_SrcLoaderURL():
 
 def Test2_SrcLoaderURL_to_documents():
     import os
-    os.environ["USER_AGENT"] = "SSRag/1.0.0"
-    sl=SrcLoaderURL()
 
-    src_param=ParameterSrcURL(  web_urls=["https://www.cnblogs.com/wlsandwho/p/18673629"],
-                                using_loader="WebBaseLoader",
-                                parse_only="cnblogs_post_body")
-    
-    res = sl.load(src_param)
+    # 保存原始 USER_AGENT 环境变量
+    original_user_agent = os.environ.get("USER_AGENT")
 
-    res2=ResultSrc(res.to_documents())
-    print(res2.src_type) 
-    print(res2.src_data)
+    try:
+        os.environ["USER_AGENT"] = "SSRag/1.0.0"
+        sl = SrcLoaderURL()
+
+        src_param = ParameterSrcURL(  web_urls=["https://www.cnblogs.com/wlsandwho/p/18673629"],
+                                    using_loader="WebBaseLoader",
+                                    parse_only="cnblogs_post_body")
+        
+        res = sl.load(src_param)
+
+        res2 = ResultSrc(res.to_documents())
+        print(res2.src_type)
+        print(res2.src_data)
+
+    finally:
+        # 恢复原始 USER_AGENT 环境变量
+        if original_user_agent is not None:
+            os.environ["USER_AGENT"] = original_user_agent
+        else:
+            os.environ.pop("USER_AGENT", None)
 
 if __name__ =='__main__':
-    #Test1_SrcLoaderURL()
+    Test1_SrcLoaderURL()
     Test2_SrcLoaderURL_to_documents() 
