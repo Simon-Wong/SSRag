26513747b6213278817f219559d1ba33b0025ec0
thbytwo <thbytwo@live.cn>
2026-06-10 13:46:50 +0800

    demo

---
+++ Splitter/SplitterBase/__init__.py
@@ -0,0 +1,1 @@
+from .splitter_base import BaseSplitter, BaseParameterSplitter, BaseResultSplitter, ResultSplitter
--- Splitter/SplitterBase/splitter_base.py
+++ Splitter/SplitterBase/splitter_base.py
@@ -13,10 +13,61 @@ src----->BaseParameterLoader
                                      BaseSplitter----->BaseResultSplitter
 
 '''
+# class BaseResultSplitter(ABC):
+#     def __init__(self):
+#         pass
+
+# class BaseSplitter(ABC):
+#     def __init__(self):
+#         pass    
+
 class BaseResultSplitter(ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
+    def to_documents(self,filter:callable=None)->list[Document]:
+        '''
+        转换为文档列表，根据filter函数过滤元素，默认返回所有元素
+        filter函数的参数为src_data中的元素，返回值为bool，True表示保留该元素，False表示过滤该元素
+        '''    
+        pass
+
+class ResultSplitter(BaseResultSplitter):
+    '''一个分割器结果基类'''
+    src_data: list[Document]|list[Element] = None # 资源文档列表
+    src_type: str = None # 资源类型
+    def __init__(self, data: list[Document]|list[Element]):
+        assert isinstance(data, list), "data must be a list"
+        self.src_data = data
+        self.src_type = next(elem.__class__.__name__ for elem in data if elem is not None) or 'Unknown'
+        #print(self.src_type)
+
+    def to_documents(self,filter:callable=None)->list[Document]:
+        '''
+        转换为文档列表，根据filter函数过滤元素，默认返回所有元素
+        filter函数的参数为src_data中的元素，返回值为bool，True表示保留该元素，False表示过滤该元素
+        '''        
+        if self.src_type == 'Document':
+            return self.src_data
+        else:
+            if filter is None:
+                filter = lambda x: True
+                
+            valid_elements = [elem for elem in self.src_data if filter(elem)]
+            if len(valid_elements) == 0:
+                return []
+            
+            docs = [Document(page_content=elem.text, metadata=elem.metadata.to_dict()) for elem in valid_elements]
+            return docs
+
+
 class BaseSplitter(ABC):
+    '''一个分割器基类'''
+    param: BaseParameterSplitter = None #参数
     def __init__(self):
-        pass    
+        pass
+
+    @abstractmethod
+    def load(self,param: BaseParameterSplitter, **kwarg)->BaseResultSplitter:
+        pass
